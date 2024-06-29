ad117e5d08c7fe7ee72f87637ef4c8e99ed5145f
Bharath Kompally <kompallybharath@gmail.com>
2024-06-29 16:40:47 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import Card from "./components/Card"
+import img from "./assets/hero.png"
+import img2 from "./assets/Imgur(1).png"
+import img3 from "./assets/Imgur (2).png"
+import img4 from "./assets/Imgur (3).png"
+import img1 from "./assets/Imgur.png"
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+     <img src={img} alt="hero img"/>
+
+      <div className='cards'>
+        <Card  url={img1} h={"Declarative"} p={"React makes it painless to create interactive UI's"}/>
+        <Card  url={img2} h={"Components"} p={"Build encapsulated components that manage their state"}/>
+        <Card  url={img3} h={"Single Way"} p={"A set of immutable values are passed to the components."}/>
+        <Card  url={img4} h={"JSX"} p={"Statically-typed designed to run on modern browsers."}/>
+        </div>
+    </div>
+  );
+}
+
+export default App;
